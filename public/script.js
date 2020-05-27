df855b56aecaf50980247fd32f7b739dce9fca64
Patrick Weaver <pjpwea@gmail.com>
2020-05-27 17:53:58 -0400

    remove unused js

--- public/script.js
+++ public/script.js
@@ -56,42 +56,6 @@ function sendReaction(projectId, reaction) {
 }
 
 
-function sendPublicFeedback(projectId, event) {
-  event.preventDefault();
-  console.log("Sending Public Feedback:")
-  var feedbackInput = document.getElementById("public-feedback-feedback");
-  var feedback = feedbackInput.value;
-  feedbackInput.value = "";
-  
-  var authorInput = document.getElementById("public-feedback-author");
-  var author = authorInput.value;
-  authorInput.value = "";
-  
-  console.log("feedback:", feedback);
-  console.log("author:", author);
-  
-  var baseUrl = `/api/${projectId}/public-feedback`;
-  var url = `${baseUrl}?author=${author}&feedback=${feedback}`;
-  
-  console.log("url:", url);
-  
-  fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    if (data.error) {
-      throw "Could save feedback."
-    }
-  })
-  .catch(error => {
-    console.log("Error: ", error);
-  })
-  
-}
-
-
 var publicFeedbackSubmit = document.getElementById("public-feedback-submit");
   if (publicFeedbackSubmit) {
   var projectId = document.getElementById("project-id").innerHTML.trim();
